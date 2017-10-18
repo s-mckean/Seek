a96eca28f1260651e53012e4f7a5a0d8cdeea6f8
Spencer McKean <s_mckean@u.pacific.edu>
2017-10-18 13:21:55 -0700

    Start of the game, two dimension

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject hidden;
+    public Light gameLighting;
+    private Color startColor;
+
+	// Use this for initialization
+	void Start () {
+        hidden.SetActive(false);
+        startColor = gameLighting.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            hidden.SetActive(true);
+            gameLighting.color = Color.black;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            hidden.SetActive(false);
+            gameLighting.color = startColor;
+        }
+	}
+
+    
+}
