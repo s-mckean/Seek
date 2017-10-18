b60f5c2b0a5fd426784a08aeb111e149c127d932
Spencer McKean <s_mckean@u.pacific.edu>
2017-10-18 16:24:52 -0700

    Added Timer to UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public GameObject hidden;
     public Light gameLighting;
     private Color startColor;
+    public Text timer;
+    public float timeRemaining = 180f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,7 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        UpdateTimer();
         if (Input.GetMouseButtonDown(1))
         {
             hidden.SetActive(true);
@@ -29,5 +33,14 @@ public class GameManager : MonoBehaviour {
         }
 	}
 
-    
+    public void UpdateTimer()
+    {
+        string secondsText;
+        int minutes = (int)timeRemaining /60;
+        int seconds = (int)timeRemaining - (60 * minutes);
+        secondsText = "" + seconds;
+        if (seconds < 10) secondsText = "0" + seconds;
+        timer.text = minutes + ":" + secondsText;
+        timeRemaining -= Time.deltaTime;
+    }
 }
